22ba527371994c542f7c17fc0640a2f8f0046e4e
PeterJoelMiller <peterjoelmiller@gmail.com>
2018-03-30 11:54:50 -0700

    Nodemailer backend setup

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+
 
 const app = express();
 const port = 3000;
+const send = require('./routes/send');  // subrouter ADD ME
+
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -15,6 +20,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
     res.send('Invalid Enpoint');
 });
+
+app.use('/send', send);     // subrouter ADD ME
 
 app.listen(port, function(){
     console.log('Server started on port ' + port);
