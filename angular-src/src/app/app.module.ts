aee79e14ce243b96fbdf8584fffc9fdd4b005c53
PeterJoelMiller <peterjoelmiller@gmail.com>
2018-04-06 09:57:56 -0700

    Added footer and performance components

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -10,19 +10,20 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { OurschoolComponent } from './components/ourschool/ourschool.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
-import { TeamComponent } from './components/team/team.component';
 import { PianoComponent } from './components/piano/piano.component';
 import { ProductionComponent } from './components/production/production.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PerformanceComponent } from './components/performance/performance.component';
 
 const appRoutes: Routes = [  // url:component routing
   {path:'', component: HomeComponent},
   {path:'ourschool', component: OurschoolComponent},
   {path:'testimonials', component: TestimonialsComponent},
-  {path:'team', component: TeamComponent},
   {path:'piano', component: PianoComponent},
   {path:'production', component: ProductionComponent},
+  {path:'performance', component: PerformanceComponent},
   {path:'contact', component: ContactComponent},
 ]
 
@@ -32,11 +33,12 @@ const appRoutes: Routes = [  // url:component routing
     HomeComponent,
     OurschoolComponent,
     TestimonialsComponent,
-    TeamComponent,
     PianoComponent,
     ProductionComponent,
     ContactComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    PerformanceComponent
   ],
   imports: [  
     BrowserModule,
