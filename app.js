9a56bcf52bf276a81a59276b33aea77022c36cb1
PeterJoelMiller <peterjoelmiller@gmail.com>
2018-04-12 12:48:11 -0700

    Changed express port number for heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 const send = require('./routes/send');  // subrouter ADD ME
 
 
