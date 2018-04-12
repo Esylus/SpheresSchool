04ea38f3ad27fe2b9dd4555a533a38298e30dce0
PeterJoelMiller <peterjoelmiller@gmail.com>
2018-04-12 12:27:51 -0700

    Change social media colors and icons

--- app.js
+++ app.js
@@ -17,17 +17,17 @@ app.use(cors());
 //set static folder to place front-end app in 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.get('/*', function (req, res, next) {
-    res.sendFile('public/index.html', {root: __dirname });
-});
-
-
-
 app.get('/', function (req, res) {
     res.send('Invalid Enpoint');
 });
 
 app.use('/send', send);     // subrouter ADD ME
+
+// Handling these redirects in Angular but below redirect worked as well
+
+app.get('/*', function (req, res, next) {
+    res.sendFile('public/index.html', {root: __dirname });
+});
 
 app.listen(port, function(){
     console.log('Server started on port ' + port);
