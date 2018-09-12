38c2498d8a7de982caaa8b8bfa34e80f2beeb450
PeterJoelMiller <peterjoelmiller@gmail.com>
2018-09-11 20:37:19 -0700

    y

--- routes/send.js
+++ routes/send.js
@@ -22,8 +22,8 @@ let transporter = nodemailer.createTransport({
     secure: false,
     port: 25,
     auth: {
-        user: process.env.NODEMAILER_USER, // your email address
-        pass: process.env.NODEMAILER_PASS // your password
+        user: 'anna@spheresschool.ca', // your email address
+        pass: 'Music2.0' // your password
     },
     tls:{
         rejectUnauthorized: false
